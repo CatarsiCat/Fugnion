44078145a6df3a9bf143241d5f1f0cd0a81e12d1
Catarsi <vashbg@gmail.com>
2019-04-03 14:23:42 +0200

    Parallax and main

--- fugnion/src/app/app.component.ts
+++ fugnion/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import {IParallaxScrollConfig} from "ng2-parallaxscroll";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'fugnion';
+  parallaxConfig: IParallaxScrollConfig = {
+    axis: 'Y',
+    speed: +.5
+  };
 }
