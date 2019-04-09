953891a2daaabcfd9d3dc0493ff59d6278438c16
Catarsi <vashbg@gmail.com>
2019-04-09 14:17:04 +0200

    Added carousel and other adjustments

--- fugnion/src/app/app.module.ts
+++ fugnion/src/app/app.module.ts
@@ -9,21 +9,21 @@ import {ModalComponent} from "./components/modal/modal.component";
 import {MenuModalComponent} from "./modules/modals/menu-modal/menu-modal.component";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
-import {AccordionModule, PanelMenuModule} from "primeng/primeng";
-import { RegisterComponent } from './components/register/register.component';
-import { HotelComponent } from './components/place/hotel/hotel.component';
-import { HowtoarriveComponent } from './components/place/howtoarrive/howtoarrive.component';
-import { TouristinfoComponent } from './components/place/touristinfo/touristinfo.component';
-import { RulebookComponent } from './components/plan/rulebook/rulebook.component';
-import { SubmitactivityComponent } from './components/plan/submitactivity/submitactivity.component';
-import { HonorguestsComponent } from './components/plan/honorguests/honorguests.component';
-import { OngComponent } from './components/plan/ong/ong.component';
-import { StuffComponent } from './components/plan/art/stuff/stuff.component';
-import { MorestuffComponent } from './components/plan/art/morestuff/morestuff.component';
-import { AbookComponent } from './components/plan/art/abook/abook.component';
-import { GalleryComponent } from './components/other/gallery/gallery.component';
-import { AboutComponent } from './components/other/about/about.component';
-import { HomeComponent } from './components/home/home.component';
+import {AccordionModule, CarouselModule, PanelMenuModule} from "primeng/primeng";
+import {RegisterComponent} from './components/register/register.component';
+import {HotelComponent} from './components/place/hotel/hotel.component';
+import {HowtoarriveComponent} from './components/place/howtoarrive/howtoarrive.component';
+import {TouristinfoComponent} from './components/place/touristinfo/touristinfo.component';
+import {RulebookComponent} from './components/plan/rulebook/rulebook.component';
+import {SubmitactivityComponent} from './components/plan/submitactivity/submitactivity.component';
+import {HonorguestsComponent} from './components/plan/honorguests/honorguests.component';
+import {OngComponent} from './components/plan/ong/ong.component';
+import {StuffComponent} from './components/plan/art/stuff/stuff.component';
+import {MorestuffComponent} from './components/plan/art/morestuff/morestuff.component';
+import {AbookComponent} from './components/plan/art/abook/abook.component';
+import {GalleryComponent} from './components/other/gallery/gallery.component';
+import {AboutComponent} from './components/other/about/about.component';
+import {HomeComponent} from './components/home/home.component';
 
 @NgModule({
   declarations: [
@@ -55,7 +55,8 @@ import { HomeComponent } from './components/home/home.component';
     BrowserModule,
     BrowserAnimationsModule,
     AccordionModule,
-    PanelMenuModule
+    PanelMenuModule,
+    CarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- fugnion/src/app/components/other/gallery/gallery.component.ts
+++ fugnion/src/app/components/other/gallery/gallery.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-gallery',
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gallery.component.scss']
 })
 export class GalleryComponent implements OnInit {
+  galleryPhotos = [];
 
-  constructor() { }
+  constructor() {
+    this.galleryPhotos = [  // In order to avoid 'Expression has changed after it was checked'
+      new CarouselMenuItem('1', 'hotel1'),
+      new CarouselMenuItem('2', 'hotel2'),
+      new CarouselMenuItem('3', 'hotel3'),
+      new CarouselMenuItem('4', 'hotel4'),
+    ];
+  }
 
   ngOnInit() {
   }
 
+}
+
+export class CarouselMenuItem { // TODO move to models folder when created
+
+  public label: string;
+  public url: string;
+
+  constructor(label: string, url: string) {
+    this.label = label;
+    this.url = url;
+  }
 }
